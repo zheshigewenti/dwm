c842ed37d67209a5803caa27efa7b84f625a1c4d
zheshigewenti <dzn1534564656@gmail.com>
2023-04-25 14:16:31 +0800

    selbgcolor

--- config.h
+++ config.h
@@ -19,15 +19,15 @@ static unsigned int gappoh    = 10;       /* horiz outer gap between windows and
 static unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 0;        /* 0 means no bar */
+static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "monospace:size=9", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true","SauceCodePro Nerd Font Mono:size=16"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#222222";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#222222";
+static char selbgcolor[]            = "#222222";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },

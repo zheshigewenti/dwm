1f6c6c3e756b3649f708ad19a48a1198a08ff37f
zheshigewenti <dzn1534564656@gmail.com>
2023-05-18 09:41:06 +0800

    fonts

--- config.h
+++ config.h
@@ -21,7 +21,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=9", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true","SauceCodePro Nerd Font Mono:size=16"  };
+static char *fonts[]          = {  "WenQuanYi Micro Hei Mono:size=9","SauceCodePro Nerd Font Mono:size=16"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#222222";
 static char normfgcolor[]           = "#bbbbbb";
